887d43248ef3a46e4f1188395e108025ddaa6576
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-07-09 14:28:13 -0400

    identify sections

--- app/quipargs.py
+++ app/quipargs.py
@@ -1,9 +1,11 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="QuIP results loader.")
+# MONGODB
 parser.add_argument("--dbhost", default="localhost", metavar="<hostname>", type=str, help="FeatureDB host name. Default: localhost")
 parser.add_argument("--dbport", default=27017, type=int, metavar="<port>", help="FeatureDB host port. Default: 27017")
 parser.add_argument("--dbname", required=True, metavar="<name>", type=str, help="FeatureDB database name.")
+# PATHDB
 parser.add_argument("--pathdb", help="use this flag if pathdb", action="store_true")
 parser.add_argument("--url", required=False, type=str, metavar="<url>", help="url of pathdb (example: https://quip.bmi.stonybrook.edu)")
 parser.add_argument("--user", required=False, type=str, metavar="<user>", help="username (if pathdb)")
